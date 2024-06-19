59ad32d28baeffd7ec355b75515671330daf63ba
sabrinacinque <cinque.sabrina@gmail.com>
2024-06-19 15:41:52 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp3/ContoCorrente.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ContoCorrente
+    {
+        string nomeTitolare { get; set; }
+        int saldo { get; set; }
+
+        bool contoAperto { get; set; }
+
+        public ContoCorrente(string nomeTitolare)
+        {
+            this.nomeTitolare = nomeTitolare;
+            this.saldo = 0;
+            this.contoAperto = false;
+        }
+
+
+        public string ApriConto(int depositoIniziale)
+        {
+
+            if (depositoIniziale >= 1000)
+            {
+                this.nomeTitolare = nomeTitolare;
+                this.saldo = depositoIniziale;
+                this.contoAperto = true;
+                return "Il conto è stato aperto a nome di " + nomeTitolare + " con un saldo di " + saldo + " euro.";
+            }
+            else
+            {
+                return "Devi depositare almeno 1000 euro per aprire il conto.";
+            }
+        }
+
+
+        public void Versa(int a)
+        {
+            if (contoAperto)
+            {
+                saldo += a;
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void Preleva(int a)
+        {
+            if (contoAperto)
+            {
+                if (saldo >= a)
+                {
+                    saldo -= a;
+                }
+                else
+                {
+                    Console.WriteLine("Non è possibile prelevare più di quanto ci sia sul conto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Non esiste un conto aperto a questo nome");
+            }
+        }
+
+
+        public override string ToString()
+        {
+            if (contoAperto)
+            {
+              return "Il saldo corrente del conto a nome di " + nomeTitolare + " è " + saldo + " euro.";
+            }
+            return "non esiste un conto aperto a questo nome";
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //esercizio 1
+            ContoCorrente conto1 = new ContoCorrente("Pippo");
+            ContoCorrente conto2 = new ContoCorrente("Paperone");
+            ContoCorrente conto3 = new ContoCorrente("Pluto");
+
+
+            Console.WriteLine(conto1.ApriConto(3000));
+            Console.WriteLine(conto2.ApriConto(2000));
+            Console.WriteLine(conto3.ApriConto(500));
+            conto3.Preleva(500);
+            Console.WriteLine(conto3.ToString());
+
+            conto1.Preleva(500);
+            Console.WriteLine(conto1.ToString());
+
+            conto1.Versa(200);
+            Console.WriteLine(conto1.ToString());
+
+
+
+            conto2.Preleva(300);
+            Console.WriteLine(conto2.ToString());
+
+            conto2.Versa(200);
+            Console.WriteLine(conto2.ToString());
+
+
+
+
+
+
+            //esercizio 2 
+
+            string[] arrayNomi = { "Lucia", "Armando", "Luca", "Francesco", "Mauro" };
+
+            string nomeDaCercare = "Lucia";
+
+            if (arrayNomi.Contains(nomeDaCercare))
+            {
+                Console.WriteLine("il nome richiesto è presente nell'array");
+            }
+            else
+            {
+                Console.WriteLine("il nome richiesto non è presente nell'array");
+            }
+
+
+            //esercizio 3
+
+            int[] arrayNumeri = { 100, 150, 135, 27, 56, 85 };
+
+            int somma = 0;
+
+
+
+            for (int i = 0; i < arrayNumeri.Length; i++)
+            {
+
+                somma += arrayNumeri[i];
+                
+            }
+
+            double mediaAritmetica = (double)somma / (arrayNumeri.Length);
+
+            Console.WriteLine("La somma dei numeri nell'array è " + somma);
+            Console.WriteLine("La media aritmetica dei numeri nell'array è " + mediaAritmetica);
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
